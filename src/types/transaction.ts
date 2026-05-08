5c4e3532ee99a86e42db86ef196f4a1048d08ae9
Lorena Alves <lorenapinalves@gmail.com>
2026-05-08 00:53:41 -0300

    projeto: fintrack inicial

---
+++ src/types/transaction.ts
@@ -0,0 +1,12 @@
+export type TransactionType = 'income' | 'expense'
+
+export interface Transaction {
+  id: string
+  user_id: string
+  type: TransactionType
+  amount: number
+  category: string
+  description: string
+  date: string
+  created_at: string
+}
